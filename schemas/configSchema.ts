bcaede8034b8d800cdc1a5c4a37387d997f734f3
Kamil <kamgre7@gmail.com>
2023-06-28 19:56:14 +0200

    Updated config schema

--- schemas/configSchema.ts
+++ schemas/configSchema.ts
@@ -3,4 +3,10 @@ import { z } from 'zod';
 export const ConfigSchema = z.object({
   urlFilePath: z.string().nonempty(),
   urlFilename: z.string().nonempty(),
+  port: z
+    .string()
+    .nonempty()
+    .transform((arg) => (isNaN(parseInt(arg)) ? 3001 : Number(arg))),
+  host: z.string().nonempty(),
+  hostName: z.string().nonempty(),
 });
