9eb34128bb8974f446233f83eb7554f1cdc4f758
Kamil <kamgre7@gmail.com>
2023-07-10 21:12:10 +0200

    Updated urlValidator

--- utils/UrlValidator.ts
+++ utils/UrlValidator.ts
@@ -2,11 +2,16 @@ import { UrlSchema } from '../schemas/urlSchema';
 
 export interface IUrlValidator {
   filterValidURL(links: string[]): string[];
+  removeWhiteSpacesAndSplit(data: string): string[];
 }
 
 export class UrlValidator implements IUrlValidator {
-  filterValidURL(links: string[]) {
+  filterValidURL(links: string[]): string[] {
     return links.filter((link) => UrlSchema.safeParse(link).success);
+  }
+
+  removeWhiteSpacesAndSplit(data: string): string[] {
+    return data.replace(/\r\n/g, '\n').split('\n');
   }
 }
 
