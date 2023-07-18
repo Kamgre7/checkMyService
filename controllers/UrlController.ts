7bf544b3d292693ca3c8d2200140e9331b7b2848
Kamil <kamgre7@gmail.com>
2023-07-18 21:02:56 +0200

    Added path to download csv file

--- controllers/UrlController.ts
+++ controllers/UrlController.ts
@@ -1,13 +1,15 @@
-import { Request, Response } from 'express';
+import { Response } from 'express';
 import { PostUrlReq } from '../schemas/postUrlSchema';
 import { IUrlService, urlService } from '../services/UrlService';
 import { PutUrlReq } from '../schemas/putUrlSchema';
 import { config } from '../config/default';
+import { GetUrlReq } from '../schemas/getUrlSchema';
+import { join } from 'path';
 
 export interface IUrlController {
   postUrl(req: PostUrlReq, res: Response): Promise<void>;
   putUrl(req: PutUrlReq, res: Response): Promise<void>;
-  downloadCsv(req: Request, res: Response): Promise<void>;
+  downloadCsv(req: GetUrlReq, res: Response): Promise<void>;
 }
 
 export class UrlController implements IUrlController {
@@ -27,8 +29,13 @@ export class UrlController implements IUrlController {
     res.status(204).end();
   };
 
-  downloadCsv = async (req: Request, res: Response): Promise<void> => {
-    res.download(config.csvFilePath, (err) => {
+  downloadCsv = async (req: GetUrlReq, res: Response): Promise<void> => {
+    const { id } = req.params;
+
+    const url = await this.urlService.getUrlById(id);
+    const path = join(config.csvFilePath, `${url}.csv`);
+
+    res.download(path, (err) => {
       if (err) {
         res.status(500).send(err.message);
       }
