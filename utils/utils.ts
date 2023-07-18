da6418a124978de2f2a8b0d47ee7375780b39813
Kamil <kamgre7@gmail.com>
2023-07-18 21:06:35 +0200

    Added function getHostName

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,3 +1,11 @@
 export const CRON_EVERY_10_SEC = '*/10 * * * * *';
 
 export const httpStatusCodeRegex = /^2\d{2}$/;
+
+export const getHostName = (link: string) => {
+  const [url, protocol, host] = link.match(
+    /^(https?:\/\/)?(.*)$/
+  ) as RegExpMatchArray;
+
+  return host;
+};
