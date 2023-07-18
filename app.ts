c31a5bf23b94d729d45421da8eb79aaf10452cd5
Kamil <kamgre7@gmail.com>
2023-07-18 21:02:38 +0200

    Added url repository init

--- app.ts
+++ app.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import { config } from './config/default';
 import { cronJob } from './utils/cronJob';
+import { urlRouter } from './routes/urlRouter';
+import { urlRepository } from './repository/UrlRepository';
 
 const app = express();
 
@@ -8,6 +10,12 @@ app.use(express.json());
 
 cronJob.start();
 
+app.use('/', urlRouter);
+
 app.listen(config.port, config.hostName, () => {
   console.log(`Listening on port ${config.host}:${config.port}`);
 });
+
+(async () => {
+  await urlRepository.init();
+})();
