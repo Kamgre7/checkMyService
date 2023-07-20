5ef9a3d73caf647be40992cc07262a1cda43f7b9
Kamil <kamgre7@gmail.com>
2023-07-20 23:25:10 +0200

    Update urlController

---
+++ src/domains/website/controllers/UrlController.ts
@@ -0,0 +1,46 @@
+import { Response } from 'express';
+import { PostUrlReq } from '../schemas/postUrlSchema';
+import { IUrlService, urlService } from '../services/UrlService';
+import { PutUrlReq } from '../schemas/putUrlSchema';
+import { config } from '../../../config/default';
+import { GetUrlReq } from '../schemas/getUrlSchema';
+import { join } from 'path';
+
+export interface IUrlController {
+  postUrl(req: PostUrlReq, res: Response): Promise<void>;
+  putUrl(req: PutUrlReq, res: Response): Promise<void>;
+  downloadCsv(req: GetUrlReq, res: Response): Promise<void>;
+}
+
+export class UrlController implements IUrlController {
+  constructor(private readonly urlService: IUrlService) {}
+
+  postUrl = async (req: PostUrlReq, res: Response): Promise<void> => {
+    const { url } = req.body;
+    const id = await this.urlService.insert(url);
+
+    res.status(200).json(id);
+  };
+
+  putUrl = async (req: PutUrlReq, res: Response): Promise<void> => {
+    const { id } = req.body;
+    await this.urlService.deactivateUrl(id);
+
+    res.status(204).end();
+  };
+
+  downloadCsv = async (req: GetUrlReq, res: Response): Promise<void> => {
+    const { id } = req.params;
+
+    const url = await this.urlService.getUrlById(id);
+    const path = join(config.csvFilePath, `${url}.csv`);
+
+    res.download(path, (err) => {
+      if (err) {
+        res.status(500).send(err.message);
+      }
+    });
+  };
+}
+
+export const urlController = new UrlController(urlService);
