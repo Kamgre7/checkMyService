31364f429d4bb2c950aa928475af2bfcefdafcb5
Kamil <kamgre7@gmail.com>
2023-07-24 21:34:07 +0200

    Removed unnecessary methods and changes methods names

--- src/domains/website/repository/UrlRepository.ts
+++ src/domains/website/repository/UrlRepository.ts
@@ -2,12 +2,11 @@ import { prisma } from '../../../db/db';
 
 export interface IUrlRepository {
   insert(url: string): Promise<string>;
-  getAllUrls(): Promise<UrlRecord[]>;
-  getActiveUrls(): Promise<UrlRecord[]>;
-  getActiveUrlsName(): Promise<string[]>;
-  getUrlByName(url: string): Promise<UrlRecord | null>;
-  getUrlById(id: string): Promise<UrlRecord | null>;
-  deactivateUrl(id: string): Promise<void>;
+  getAll(): Promise<UrlRecord[]>;
+  getActive(): Promise<UrlRecord[]>;
+  getById(id: string): Promise<UrlRecord | null>;
+  getByContext(context: Partial<UrlRecord>): Promise<UrlRecord | null>;
+  updateByContext(id: string, context: Partial<UrlRecord>): Promise<void>;
 }
 
 export interface UrlRecord {
@@ -17,60 +16,54 @@ export interface UrlRecord {
 }
 
 export class UrlRepository implements IUrlRepository {
-  constructor() {}
+  constructor(private readonly urlList = prisma.urlList) {}
 
   async insert(url: string): Promise<string> {
-    const record = await prisma.urlList.create({
+    const record = await this.urlList.create({
       data: {
         url,
+        isActive: true,
       },
     });
 
     return record.id;
   }
 
-  async getAllUrls(): Promise<UrlRecord[]> {
-    return prisma.urlList.findMany();
+  async getAll(): Promise<UrlRecord[]> {
+    return this.urlList.findMany();
   }
 
-  async getActiveUrls(): Promise<UrlRecord[]> {
-    return prisma.urlList.findMany({
+  async getActive(): Promise<UrlRecord[]> {
+    return this.urlList.findMany({
       where: {
         isActive: true,
       },
     });
   }
 
-  async getActiveUrlsName(): Promise<string[]> {
-    const result = await this.getActiveUrls();
-
-    return result.map(({ url }) => url);
-  }
-
-  async getUrlById(id: string): Promise<UrlRecord | null> {
-    return prisma.urlList.findUnique({
+  async getById(id: string): Promise<UrlRecord | null> {
+    return this.urlList.findUnique({
       where: {
         id,
       },
     });
   }
 
-  async getUrlByName(url: string): Promise<UrlRecord | null> {
-    return prisma.urlList.findUnique({
-      where: {
-        url,
-      },
+  async getByContext(context: Partial<UrlRecord>): Promise<UrlRecord | null> {
+    return this.urlList.findFirst({
+      where: context,
     });
   }
 
-  async deactivateUrl(id: string): Promise<void> {
-    await prisma.urlList.update({
+  async updateByContext(
+    id: string,
+    context: Partial<UrlRecord>
+  ): Promise<void> {
+    await this.urlList.update({
       where: {
         id,
       },
-      data: {
-        isActive: false,
-      },
+      data: context,
     });
   }
 }
